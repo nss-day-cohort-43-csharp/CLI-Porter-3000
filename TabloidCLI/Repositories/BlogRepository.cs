8584c4200b19d20154a2809217790d57d1d414b6
Faith Magras <bdanielle092@gmail.com>
2020-12-07 11:21:16 -0600

    you can view all the blogs

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.Repositories
+{
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetAll()
+        {
+            using(SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Title, Url FROM Blog";
+
+
+                    List<Blog> blogs = new List<Blog>();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+
+                    while(reader.Read())
+                    {
+                        Blog blog = new Blog()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Url = reader.GetString(reader.GetOrdinal("Url"))
+                        };
+
+                        blogs.Add(blog);
+                    }
+                    reader.Close();
+                    return blogs;
+                }
+            }
+        }
+
+        public void Insert(Blog entry)
+        {
+            
+        }
+
+        public void Update(Blog entry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+     
+   
+
+}
---
+++ TabloidCLI/UserInterfaceManagers/BlogManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class BlogManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepository;
+        private string _connectionString;
+
+        public BlogManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _blogRepository = new BlogRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Blog Menu");
+            Console.WriteLine("1) List Blogs");
+            Console.WriteLine("2) Add Blog");
+            Console.WriteLine("3) Edit Blog");
+            Console.WriteLine("4) Remove Blog");
+            Console.WriteLine("0) Go Bakc");
+
+            Console.Write(">");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Blog> blogs = _blogRepository.GetAll();
+            foreach (Blog blog in blogs)
+            {
+                Console.WriteLine(blog);
+            }
+        }
+    }
+}
